2191021c7ee6a79906f14cbe679e3cd62c9a66a7
devjinx <t.chareonlertkamol@gmail.com>
2023-10-08 13:22:42 +0700

    PageNotFound Update

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import Register from '../views/Register.vue'
 import Login from '../views/Login.vue'
 import Store from '../views/Store.vue'
 import Topup from '../views/Topup.vue'
-
+import PageNotFound from '../views/PageNotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -33,8 +33,12 @@ const router = createRouter({
       path: '/store',
       name: 'store',
       component: Store
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: PageNotFound
     }
-
   ]
 })
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,18 +1,29 @@
 <template>
-    <div>
-      <h2>Login</h2>
-      <form @submit.prevent="login">
-        <div class="form-group">
-          <label for="username">Username:</label>
-          <input type="text" id="username" v-model="username" required />
-        </div>
-        <div class="form-group">
-          <label for="password">Password:</label>
-          <input type="password" id="password" v-model="password" required />
-        </div>
-        <button type="submit">Login</button>
+  <center>
+    <div class="wrapper">
+      <form action="">
+          <h1>Login </h1>
+          <div class="input-box">
+              <input type="text" placeholder="Username" required>
+          </div>
+          <div class="input-box">
+              <input type="password" placeholder="Password" required>
+          </div>
+          <div class="remember-forgot">
+              <label>
+                  <input type="checkbox">Remember me
+              </label>
+              <a href="#">Forgot password?</a>
+          </div>
+          <button type="submit" class="btn">Login</button>
+  
+          <div class="register-link">
+            <p>Don't have an account? <router-link to="/register">Register</router-link></p>
+          </div>
       </form>
-    </div>
+  </div>
+  </center>
+
   </template>
   
   <script>
@@ -34,6 +45,98 @@
   </script>
   
     <style scoped>
-
+    *{
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  }
+  body{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      min-height: 100vh;
+  }
+  .wrapper{
+      width: 420px;
+      background: transparent;
+      border:2px solid rgba(255, 255, 255, .2);
+      backdrop-filter: blur(20px);
+      box-shadow: 0 0 10px rgba(0, 0, 0, .2);
+      color: #fff;
+      border-radius: 10px;
+      padding: 30px 40px;
+  }
+  .wrapper h1{
+      font-size: 36px;
+      text-align: center;
+  }
+  .wrapper .input-box{
+      width: 100%;
+      height: 60px;
+      position: relative;
+      margin-top: 20px;
+  }
+  .input-box input{
+      width: 100%;
+      height: 100%;
+      background: transparent;
+      border: none;
+      outline: none;
+      border: 2px solid rgba(255, 255, 255, .2);
+      border-radius: 40px;
+      font-size: 16px;
+      color: #fff;
+      padding: 20px 40px 20px 20px;
+  }
+  .input-box input::placeholder{
+      color: #fff;
+  
+  }
+  .wrapper .remember-forgot{
+      display: flex;
+      justify-content: space-between;
+      font-size: 14.5px;
+      margin: 15px 5px 15px;
+  }
+  .remember-forgot label input{
+  accent-color: #fff;
+  margin-right: 3px;
+  }
+  .remember-forgot a {
+      color: #fff;
+      text-decoration: none;
+  }
+  .remember-forgot a:hover{
+      text-decoration: none;
+      color: black;
+  }
+  .wrapper .btn{
+      width: 100%;
+      height: 45px;
+      background: #fff;
+      border: none;
+      outline: none;
+      border-radius: 40px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, .1);
+      cursor: pointer;
+      color: #333;
+      font-weight: 700;
+  }
+  .wrapper .register-link{
+      font-size: 14.5px;
+      text-align: center;
+      margin: 20px 0 15px;
+  
+  }
+  .register-link p a{
+      color: #fff;
+      text-decoration: none;
+      font-weight: 600;
+  }
+  .register-link p a:hover{
+      text-decoration: none;
+      color: black;
+  }
     </style>
   
