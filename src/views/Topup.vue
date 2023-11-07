7427bcabc5c72c5c22e658d3d64f8e66276cfb91
devjinx <t.chareonlertkamol@gmail.com>
2023-11-07 09:25:50 +0700

    update topup

--- src/views/Topup.vue
+++ src/views/Topup.vue
@@ -3,12 +3,22 @@
     <section id="lamyPageTop" class="background-image">
       <div class="contentTop">
         <img src="../assets/Lamy_logo_png.png" alt="logolamy" height="300" width="300">
-          <h1>เติมเงิน</h1>
-          <h2>เลือกช่องทางการเติมเงินที่คุณต้องการ</h2>
+        <h1>เติมเงิน</h1>
+        <h2>เลือกช่องทางการเติมเงินที่คุณต้องการ</h2>
       </div>
     </section>
-    <section id="lamyService">
-      <h3>test</h3>
+    <section id="lamyServiceTop">
+      <div class="button-container">
+        <button class="button">
+          <img src="../assets/money-transfer.png" height="100" width="100">
+        </button>
+        <button class="button">
+          <img src="../assets/anupao_truewall-removebg-preview.png" height="100" width="100">
+        </button>
+        <button class="button">
+          <img src="../assets/truemoneywallet.png" height="100" width="100">
+        </button>
+      </div>
     </section>
   </div>
 </template>
@@ -21,14 +31,14 @@ export default {
 
 <style>
 .homeTop {
-  padding-top: 40px;    
+  padding-top: 40px;
   padding-bottom: 60px;
   font-family: 'Kanit', sans-serif;
   display: flex;
   flex-direction: column;
-  margin: 0; 
-  padding: 0; 
-  overflow: hidden; 
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
   margin: 0%;
   padding: 0%;
 }
@@ -45,21 +55,21 @@ export default {
   background-size: cover;
   width: 100%;
   height: 100%;
-  position: relative; 
+  position: relative;
 }
 
 .contentTop {
   display: flex;
   flex-direction: column;
   align-items: center;
-  position: relative; 
+  position: relative;
 }
 
 #lamyPageTop img {
   display: block;
   margin: 0 auto;
-  width: 250px;  
-  height: 250px; 
+  width: 250px;
+  height: 250px;
   opacity: 0.4;
   position: absolute;
   top: 50%;
@@ -71,13 +81,13 @@ export default {
 #lamyPageTop h1 {
   font-size: 32px;
   z-index: 2;
-  margin: 0; 
+  margin: 0;
 }
 
 #lamyPageTop h2 {
   font-size: 24px;
   z-index: 2;
-  margin: 0; 
+  margin: 0;
 }
 
 #lamyPageTop .header-text {
@@ -86,18 +96,39 @@ export default {
 }
 
 #lamyPageTop .header-text h1 {
-  margin-right: 10px; 
+  margin-right: 10px;
 }
 
-#lamyService {
+#lamyServiceTop {
   background: white;
   color: initial;
   padding: 20px;
-  min-height: 100vh;
+  min-height: 30vh;
   text-align: center;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+}
+
+#lamyServiceTop .button-container {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-wrap: wrap;
+  margin-bottom: 20%;
+}
+
+#lamyServiceTop .button {
+  width: 400px;
+  height: 250px;
+  background-color: #ffffff;
+  border: 1px solid #e0e0e0;
+  margin: 20px;
+  flex: 0 0 auto;
+  text-align: center;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+  cursor: pointer;
 }
 </style>
