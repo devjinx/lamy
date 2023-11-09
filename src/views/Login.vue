d785464cdd743d1012596a5fb231b28f9a1383ad
devjinx <t.chareonlertkamol@gmail.com>
2023-11-09 20:44:38 +0700

    update login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,22 +1,28 @@
 <template>
-  <form class="login-form" @submit.prevent="login">
-    <div class="centered">
-      <h1>Login</h1>
+  <form class="login-form needs-validation" @submit.prevent="login" novalidate>
+    <div class="text-center mb-4">
+      <h1 class="h3 mb-3 font-weight-normal">เข้าสู่ระบบ</h1>
     </div>
-    <label for="username">Username:</label>
-    <input type="text" id="username" v-model="username" name="username" required /><br /><br />
-    <label for="password">Password:</label>
-    <input type="password" id="password" v-model="password" name="password" required /><br /><br />
-    <div v-if="errorMessages.length" class="error-messages">
+    <div class="form-group">
+      <label for="username">ชื่อผู้ใช้:</label>
+      <input type="text" id="username" v-model="username" name="username" class="form-control" required />
+      <div class="invalid-feedback">โปรดใส่ชื่อผู้ใช้</div>
+    </div>
+    <div class="form-group">
+      <label for="password">รหัสผ่าน:</label>
+      <input type="password" id="password" v-model="password" name="password" class="form-control" required />
+      <div class="invalid-feedback">โปรดใส่รหัสผ่าน</div>
+    </div>
+    <div v-if="errorMessages.length" class="alert alert-danger">
       <ul>
         <li v-for="message in errorMessages" :key="message">{{ message }}</li>
       </ul>
     </div>
-    <div class="centered">
-      <div class="form-buttons">
-        <input type="submit" value="Login" class="login-button" />
-        <router-link to="/register" class="back-to-register-button">Register ?</router-link>
-      </div>
+    <div class="form-group">
+      <button type="submit" class="btn btn-primary btn-block" style="background-color: #0ea5e9;">เข้าสู่ระบบ</button>
+    </div>
+    <div class="form-group">
+      <router-link to="/register" class="btn btn-secondary btn-block mt-3" style="background-color: #0ea5e9;">สร้างบัญชี</router-link>
     </div>
   </form>
 </template>
@@ -50,7 +56,7 @@ export default {
       // Store the token, e.g., in local storage or Vuex state
       localStorage.setItem('authToken', response.data.token);
 
-      alert('Login successful');
+      alert('เข้าสู่ระบบสำเร็จ');
       this.$router.push('/');
       // Redirect to the home page when login is successful
     } else {
@@ -74,82 +80,63 @@ export default {
 <style>
 body {
   font-family: 'Kanit', sans-serif;
-  text-align: center;
-  margin: 0;
-  padding: 0;
   background-color: #f2f2f2;
 }
 
 .login-form {
-  font-family: 'Kanit', sans-serif;
-  background-color: #ffffff;
+  background-color: white;
   max-width: 400px;
   margin: 0 auto;
   padding: 100px;
   border-radius: 8px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  text-align: left;
+  box-shadow: 0 0 80px rgba(0, 0, 0, 0.1);
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
 }
 
-label {
-  font-family: 'Kanit', sans-serif;
-  display: block;
-  font-weight: bold;
-  margin-bottom: 8px;
+.login-form h1 {
+  margin-bottom: 20px;
 }
 
-input[type="text"],
-input[type="password"] {
-  width: 100%;
-  padding: 10px;
+.login-form input {
   margin-bottom: 15px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  font-size: 16px;
+  text-align: left; 
 }
 
-input[type="submit"] {
-  font-family: 'Kanit', sans-serif;
-  margin-left: 20px;
-  background-color: #007bff;
-  color: #fff;
-  padding: 10px 20px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  font-size: 18px;
+.login-form label {
+  display: block; 
+  text-align: left;
 }
 
-input[type="submit"]:hover {
-  background-color: #0056b3;
-}
-
-.back-to-register-button {
-  font-family: 'Kanit', sans-serif;
-  margin-left: 20px;
-  padding: 10px 20px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  font-size: 18px;
-  background-color: #007bff;
-  color: #fff;
-  text-decoration: none;
-}
-
-.centered {
-  text-align: center;
+.login-form button, .login-form .btn-secondary {
+  margin-top: 15px;
+  text-align: left; 
 }
 
 .error-messages {
   color: #ff0000;
 }
+@media only screen and (max-width: 600px) {
+  .login-form h1 {
+    font-size: 20px; 
+  }
+  .login-form input {
+    font-size: 15px;
+    margin-bottom: 10px;
+    padding: 10px;
+  }
+  .login-form {
+    max-width: 90%; 
+    padding: 60px;
+  }
+  .login-form label {
+    font-size: 15px;
+}
 
-.login-form {
-  padding-top: 50px;
+  .login-form button, .login-form .btn-secondary {
+    font-size: 15px; 
+  }
 }
 </style>
